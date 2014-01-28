10581efd3726314afa3b6f220fa9ae56b41b96e0
slowjack2k <shad0wrunner@gmx.de>
2014-01-28 12:35:46 +0100

    added a all script to examples

---
+++ examples/all_examples.rb
@@ -0,0 +1,7 @@
+Dir[File.join(File.expand_path(__dir__ ), "*.rb")].sort.each do |f|
+  unless f =~ /#{__FILE__}$/
+    command = %W(ruby #{f})
+    puts "executing #{command.join(" ")}"
+    system *command
+  end
+end
